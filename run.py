62dacd77a76d5628a3dd41a76d2bf3ed1b135a21
Emil <972080809@qq.com>
2019-09-25 14:54:37 +0800

    修改 run.py

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ from listener import RepoUpdateListener
 
 username = 'hvnobug'
 
-# 可以生成 仓库相关 github api
 # 可以生成 用户相关 github api
 gu = GithubUser(username)
 
+# 监听器列表
 listeners = []
 
 
@@ -22,15 +22,18 @@ def start_listen():
     """
     监听仓库更新
     """
+    # 获取用户 starred 仓库列表
     result = requests.get(gu.starred_url()).json()
     for repository in result:
         repo = repository['name']
         user = repository['owner']['login']
+        # 可以生成 仓库相关 github api
         gur = GithubUserRepository(user, repo)
         time.sleep(10)
+        # 监听仓库更新并发送邮件通知
         listener = RepoUpdateListener(github_repo=gur, duration=600)
-        listener.start()
         listeners.append(listener)
+        listener.start()
 
 
 def pause_listen():
